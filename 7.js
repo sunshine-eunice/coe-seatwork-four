71543519bcec5fc20499c60e2132fa771ac77809
sunshine-eunice <sunshineeunicec@yahoo.com>
2014-08-09 14:47:01 +0800

    updated code to 7

--- 7.js
+++ 7.js
@@ -1,7 +1,15 @@
-function convertFirstLetter(a){
-	var b = a.split(" "),c = "";
-	for(var d in b){
-		c += (b[d].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) + " ");
+function mostFrequent(a){    
+	var b = {};  
+	var c = 0;  
+	var d;   
+	for(var e in a) {
+        b[a[e]]=(b[a[e]] || 0)+1; 
+        if(b[a[e]] > c) { 
+                c = b[a[e]];  
+                d = a[e];     
+        }
 	}
-	return c.substring(0,c.length-1);
+	return d + " ( " + c + " times )";
 }
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+mostFrequent(arr1);
